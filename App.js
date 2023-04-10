b681539a37dec0ae08dd9e3c592e25ac3f8114aa
sudhanshusingh1 <83897540+sudhanshusingh1@users.noreply.github.com>
2023-04-10 18:44:24 +0530

    SRC

---
+++ App.js
@@ -0,0 +1,57 @@
+
+import './App.css';
+
+function App() {
+  return (
+<>
+
+<nav className="navbar navbar-expand-lg bg-body-tertiary"  data-bs-theme="dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">DTDC</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="/">Home</a>
+        <a className="nav-link" href="/">Features</a>
+        <a className="nav-link" href="/">Pricing</a>
+        
+      </div>
+    </div>
+  </div>
+</nav>
+
+<div id="carouselExampleIndicators" className="carousel slide">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="..." className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="..." className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="..." className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+</>
+  );
+}
+
+export default App;
